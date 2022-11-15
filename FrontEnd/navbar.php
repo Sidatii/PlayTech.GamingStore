bdfc01a1f4350205142f9ecc8ca49d195d2d5f80
sidatii <sidatnouhi@gmail.com>
2022-11-15 22:14:15 +0100

    tiny additions

--- FrontEnd/navbar.php
+++ FrontEnd/navbar.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PlayTech | Gallery</title>
+    <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
@@ -18,12 +18,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link text-light " aria-current="page" href="index.php">Home</a>
+          <a class="nav-link text-primary" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-primary" href="Gallery.php">Gallery</a>
+          <a class="nav-link text-light" href="Gallery.php">Gallery</a>
         </li>
         <li class="nav-item">
+          
           <a class="nav-link text-light" href="Page Gestion.php">Gestion de Produits</a>
         </li>
       </ul>
