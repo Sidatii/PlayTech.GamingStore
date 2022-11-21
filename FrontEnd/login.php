8ca73189e3e437fac9b4eb9b303251d83dc262fa
MokhlisBelhaj <belhajmokhlis0@gmail.com>
2022-11-21 09:15:03 +0100

    AFFICHER

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -22,11 +22,11 @@
             <img class="mb-4" src="images/Asset 1PlayTech_logo.svg" alt="" width="72" height="57">
             <h1 class="h3 mb-3">Please sign in</h1>
 
-            <div class="form-floatingr">
+            <div class="form-floating">
                 <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
                 <label for="floatingInput">Email address</label>
             </div>
-            <div class="form-floatingr">
+            <div class="form-floating">
                 <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
                 <label for="floatingPassword">Password</label>
             </div>
