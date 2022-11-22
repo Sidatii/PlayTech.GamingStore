e0c938bcbe066523d70cde84b2adf4141a3ae378
MokhlisBelhaj <belhajmokhlis0@gmail.com>
2022-11-22 19:33:38 +0100

    img

---
+++ FrontEnd/modifier.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter</title>
+    <link rel="stylesheet" href="ajouter.css">
+</head>
+<body>
+<?php
+
+         //connexion à la base de donnée
+          include_once "connexion.php";
+         //on récupère le id dans le lien
+          $id = $_GET['ID'];
+          //requête pour afficher les infos d'un employé
+          $req = mysqli_query($conn , "SELECT * FROM produits WHERE ID = $id");
+          $row = mysqli_fetch_assoc($req);
+
+
+       //vérifier que le bouton ajouter a bien été cliqué
+       if(isset($_POST['button'])){
+           //extraction des informations envoyé dans des variables par la methode POST
+           extract($_POST);
+           //verifier que tous les champs ont été remplis
+           if(isset($nom) && isset($prenom) && $age){
+               //requête de modification
+               $req = mysqli_query($con, "UPDATE employe SET nom = '$nom' , prenom = '$prenom' , age = '$age' WHERE id = $id");
+                if($req){//si la requête a été effectuée avec succès , on fait une redirection
+                    header("location: index.php");
+                }else {//si non
+                    $message = "Employé non modifié";
+                }
+
+           }else {
+               //si non
+               $message = "Veuillez remplir tous les champs !";
+           }
+       }
+    
+    ?>
+
+    <div class="form">
+        <a href="index.php" class="back_btn"><img src="images/back.png"> Retour</a>
+        <h2>Modifier l'employé : <?=$row['nom']?> </h2>
+        <p class="erreur_message">
+           <?php 
+              if(isset($message)){
+                  echo $message ;
+              }
+           ?>
+        </p>
+        <form action="" method="POST">
+            <label>Nom</label>
+            <input type="text" name="nom" value="<?=$row['']?>">
+            <label>Prénom</label>
+            <input type="text" name="prenom" value="<?=$row['prenom']?>">
+            <label>âge</label>
+            <input type="number" name="age" value="<?=$row['age']?>">
+            <input type="submit" value="Modifier" name="button">
+        </form>
+    </div>
+</body>
+</html>
