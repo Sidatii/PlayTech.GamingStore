9bab12d9408b37542eff18eb2732ce0a1a32e8c2
MokhlisBelhaj <belhajmokhlis0@gmail.com>
2022-12-09 14:45:11 +0100

    supConn

--- FrontEnd/supprimer.php
+++ FrontEnd/supprimer.php
@@ -1,10 +1,11 @@
 <?php
   //connexion a la base de données
-  include_once "dbconnexion.php";
+  require 'dbconnection.php';
   //récupération de l'id dans le lien
   $id= $_GET['ID'];
   //requête de suppression
   $req = mysqli_query($con , "DELETE FROM produits WHERE ID = $id");
   //redirection vers la page index.php
+  //
   header("Location:GestionProduits.php")
 ?>
